8e3268d17fd260556d74be4325418822bc2817f2
ArthurA14 <77979858+ArthurA14@users.noreply.github.com>
2022-06-14 20:45:40 +0200

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -16,6 +16,47 @@ app.config.from_object('config.Config')
 
 @app.route('/', methods=['GET'])
 def home():
+    html = """
+           <h1>Sunrise-sunset API</h1>
+           <p>Example RESTful API for the course Lab. of Cloud Computing, Big Data and security @ UniCatt</p>
+           """
+    return html
+
+
+@app.route('/api/prova/<city>', methods=['GET'])
+def api_prova(city):
+    output = {}
+    if city:
+        output = {
+            'sunrise': 1,
+            'sunset': 2,
+            'city': city
+        }
+    return jsonify(output)
+
+
+@app.route('/api/daylight/<city>', methods=['GET'])
+def api_daylight(city):
+    output = {}
+    uri = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={config.Config.APIKEY}"
+    # uri = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKEY}"
+    print(uri)
+    res = requests.get(uri)
+    if res.status_code == 200:
+        data = res.json()
+        sunrise = data['sys']['sunrise']
+        sunset = data['sys']['sunset']
+        output = {
+            'sunrise': sunrise,
+            'sunset': sunset,
+            'daylight': sunset-sunrise,
+            'city': city            
+        }
+    return output
+
+
+@app.route('/', methods=['GET'])
+def api_daylight2():
     output = {}
     uri = f"https://api.openweathermap.org/data/2.5/weather?lat={config.Config.LAT}&lon={config.Config.LONG}&appid={config.Config.APIKEY}"
     # uri = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LONG}&appid={APIKEY}"
@@ -54,5 +95,5 @@ def home():
     return html
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8081)
+    app.run(debug=True, host='0.0.0.0', port=8081)
 # app.run(host='0.0.0.0')  # accept connection from every host
