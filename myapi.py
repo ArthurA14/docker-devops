6e496247cd191297abb6c3969af6fe15187c65a5
ArthurA14 <77979858+ArthurA14@users.noreply.github.com>
2022-06-14 21:23:08 +0200

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -81,16 +81,16 @@ def api_daylight2():
             <h1>API TP DEVOPS - Arthur ALLIE </h1>
             <p><b> Latitude </b> : """+ str(latitude) +""" °</p>
             <p><b> Longitude </b> : """+ str(longitude) +""" °</p>
-            <p><b> Pays </b> : """+ str(country) +"""</p>
-            <p><b> Ville </b> : """+ str(city) +"""</p>
-            <p><b> Température </b> :"""+ str(temp) +""" °C</p>
-            <p><b> Température ressentie </b> : """+ str(feels) +""" °C</p>
-            <p><b> Temps </b> : """+ str(meteo) +"""</p>
-            <p><b> Description du temps </b> : """+ str(descri) +"""</p>
-            <p><b> Force du vent </b> : """+ str(wind) +"""</p>
-            <p><b> Heure du lever du soleil </b> : """+ str(sunrise) +"""</p>
-            <p><b> Heure du coucher du soleil </b> : """+ str(sunset) +"""</p>
-            <p><b> Durée de soleil </b> : """+ str(daylight) +"""</p>"""
+            <p><b> Pays </b> : """+ str(country) +""" </p>
+            <p><b> Ville </b> : """+ str(city) +""" </p>
+            <p><b> Température </b> :"""+ str(temp) +""" </p>
+            <p><b> Température ressentie </b> : """+ str(feels) +""" </p>
+            <p><b> Temps </b> : """+ str(meteo) +""" </p>
+            <p><b> Description du temps </b> : """+ str(descri) +""" </p>
+            <p><b> Force du vent </b> : """+ str(wind) +""" </p>
+            <p><b> Heure du lever du soleil </b> : """+ str(sunrise) +""" </p>
+            <p><b> Heure du coucher du soleil </b> : """+ str(sunset) +""" </p>
+            <p><b> Durée de soleil </b> : """+ str(daylight) +""" </p>"""
             
     return html
 
