3cb6457e8f23f7ae89c02b0d79a485c9c0b99e30
ArthurA14 <77979858+ArthurA14@users.noreply.github.com>
2022-06-14 20:36:11 +0200

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -54,5 +54,5 @@ def home():
     return html
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8081)
+    app.run(debug=True, host='0.0.0.0', port=8081)
 # app.run(host='0.0.0.0')  # accept connection from every host
