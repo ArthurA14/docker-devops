7d9bc14ab2ee8b3825b3379dc497b60eee50b281
ArthurA14 <77979858+ArthurA14@users.noreply.github.com>
2024-02-29 23:38:41 +0100

    Update prova_api.py

--- prova_api.py
+++ prova_api.py
@@ -2,13 +2,19 @@ import requests
 import os
 
 APIKEY = os.environ['APIKEY']  # reads the environment variable
-LAT = os.environ['LAT']
-LONG = os.environ['LONG']
+# LAT = os.environ['LAT']
+# LONG = os.environ['LONG']
+CITY = os.environ['CITY']
 
-# response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=brescia&appid="+APIKEY)
-url="https://api.openweathermap.org/data/2.5/weather?lat="+LAT+"&lon="+LONG+"appid="+APIKEY
-# print(url)
-response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+LAT+"&lon="+LONG+"&appid="+APIKEY)
+# url_1 = "https://api.openweathermap.org/data/2.5/weather?q=monaco&appid="+APIKEY
+# url_2 = "https://api.openweathermap.org/data/2.5/weather?lat="+LAT+"&lon="+LONG+"&appid="+APIKEY
+# print(url_2)
+url_3 = "https://api.openweathermap.org/data/2.5/weather?q="+CITY+"&appid="+APIKEY
+
+# response = requests.get(url_1)
+# response = requests.get(url_2)
+response = requests.get(url_3)
                          
 print(response.status_code)
 print(response.json())
+# print(os.environ.get('APIKEY'))
